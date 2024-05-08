4c04cf3780b06313c3a999c1fcdbe14a3178d3a5
Kaopong Sangkittiyakorn <84172946+kuisskui@users.noreply.github.com>
2024-05-08 17:21:17 +0700

    Initial Commit

---
+++ Spending Tracker/Spending_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Spending_TrackerApp.swift
+//  Spending Tracker
+//
+//  Created by kuisskui on 8/5/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct Spending_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
